40e003a4e670dfac2a430a518272e066fd6b51e9
Mohammad Shahbaz <mdshahbaz7896@gmail.com>
2024-11-01 17:11:19 +0530

    update

---
+++ app/src/main/java/com/example/geminichatapp/ChatData.kt
@@ -0,0 +1,140 @@
+package com.example.geminichatapp
+
+import android.graphics.Bitmap
+import com.example.geminichatapp.data.Chat
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.Content
+import com.google.ai.client.generativeai.type.ResponseStoppedException
+import com.google.ai.client.generativeai.type.content
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+
+interface ChatDataFromModel {
+
+    suspend fun getResponseByPrompt( prompt : String) : Chat
+    suspend fun getResponseByPromptAndBitmap( prompt : String , bitmap: Bitmap) : Chat
+}
+
+
+class ChatData() : ChatDataFromModel {
+
+    val api_key = "AIzaSyBIbim7UzQvS2L6uOYEqMX1b2K5LLqB6MQ"
+
+    override suspend fun getResponseByPrompt(prompt: String): Chat {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro",
+            apiKey = api_key
+        )
+
+        try {
+            val response  = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(prompt)
+            }
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }catch (e : ResponseStoppedException) {
+            return Chat(
+                prompt = e.message ?: " error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+    }
+
+    override suspend fun getResponseByPromptAndBitmap(prompt: String, bitmap: Bitmap): Chat {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro-vision",
+            apiKey = api_key
+        )
+
+        val inputContent = content {
+            image(bitmap)
+            text(prompt)
+        }
+
+        try {
+            val response = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(inputContent)
+            }
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }catch (e:ResponseStoppedException) {
+            return Chat(
+                prompt = e.message ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+    }
+
+}
+
+
+
+
+/*
+object ChatData {
+
+
+
+    suspend fun getResponseByPrompt( prompt : String) : Chat {
+
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro",
+            apiKey = api_key
+        )
+
+        try {
+            val response  = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(prompt)
+            }
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }catch (e : ResponseStoppedException) {
+            return Chat(
+                prompt = e.message ?: " error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+    }
+    suspend fun getResponseByPromptAndBitmap( prompt : String , bitmap: Bitmap) :Chat {
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-pro-vision",
+            apiKey = api_key
+        )
+
+        val inputContent = content {
+            image(bitmap)
+            text(prompt)
+        }
+
+        try {
+            val response = withContext(Dispatchers.IO) {
+                generativeModel.generateContent(inputContent)
+            }
+            return Chat(
+                prompt = response.text ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }catch (e:ResponseStoppedException) {
+            return Chat(
+                prompt = e.message ?: "error",
+                bitmap = null,
+                isFromUser = false
+            )
+        }
+    }
+}
+*/
---
+++ app/src/main/java/com/example/geminichatapp/ChatUiEvent.kt
@@ -0,0 +1,13 @@
+package com.example.geminichatapp
+
+import android.graphics.Bitmap
+
+sealed class ChatUiEvent {
+
+    data class UpdatePrompt(val newPrompt : String) :ChatUiEvent()
+    data class UpdateBitmap(val newBitmap : Bitmap) : ChatUiEvent()
+    data class SendPrompt(
+        val prompt : String,
+        val bitmap : Bitmap?
+    ) : ChatUiEvent()
+}
